2d17e770f751bcbc76369c3d230f923dc67ff7f9
Victor Saprykin <victanik@mail.ru>
2025-07-13 19:52:24 +0300

    fix: icons svg sprite

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const navbar = document.querySelector('.navbar');
+const logo = document.querySelector('.logo-svg use');
 window.addEventListener('scroll', () => {
   if (this.scrollY > 5) {
-    navbar.classList.add('navbar-light')
+    navbar.classList.add('navbar-light');
+    logo.href.baseVal = "img/sprite.svg#logo-dark";
+    
   } else {
     
-      navbar.classList.remove('navbar-light')
+      navbar.classList.remove('navbar-light');
+    logo.href.baseVal = "img/sprite.svg#logo-light";
   }
 });
+
+
+
 
