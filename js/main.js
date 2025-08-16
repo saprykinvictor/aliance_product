5d43b8ce27ed980f8c1e1639e02aee1640c8fcb7
Victor Saprykin <victanik@mail.ru>
2025-08-16 18:13:33 +0300

    fix: about section

--- js/main.js
+++ js/main.js
@@ -3,18 +3,19 @@ const logoLight = document.querySelector('.logo-light');
 const logo = document.querySelector('.logo');
 const mobMenuToggle = document.querySelector('.mobile-menu-toggle');
 const menu = document.querySelector('.mobile-menu');
+const isFront = document.body.classList.contains('front-page');
 
 const lightModeOn = (event) => {
   navbar.classList.add('navbar-light');
-  logo.style.display = 'block';
-  logoLight.style.display = 'none';
 };
 
 const lightModeOff = (event) => {
   navbar.classList.remove('navbar-light');
-  logo.style.display = 'none';
-  logoLight.style.display = 'block';
 };
+
+const changeNavHeight = (height) => {
+  navbar.style.height = height;
+}
 
 const openMenu = (event) => {
   menu.classList.add('is-open');
@@ -31,7 +32,10 @@ const closeMenu = (event) => {
 };
 
 window.addEventListener('scroll', () => {
-  this.scrollY > 5 ? lightModeOn() : lightModeOff();
+  this.scrollY > 5 ? changeNavHeight('74px') : changeNavHeight('94px');
+  if (isFront) {
+    this.scrollY > 5 ? lightModeOn() : lightModeOff();
+  }
 });
 
 mobMenuToggle.addEventListener('click', (event) => {
@@ -61,6 +65,49 @@ const swiperSteps = new Swiper('.steps-slider', {
     prevEl: '.steps-button-prev',
   },
 });
+
+const swiperExperts = new Swiper('.experts-slider', {
+  speed: 700,
+  loop: true,
+  // slidesPerView: 1,
+
+  // breakpoints: {
+  //   576: {
+  //     slidesPerView: 2,
+  //   },
+  //   768: {
+  //     slidesPerView: 3,
+  //   },
+  //   1024: {
+  //     slidesPerView: 5,
+  //   },
+  // },
+
+  navigation: {
+    nextEl: '.experts-button-next',
+    prevEl: '.experts-button-prev',
+  },
+});
+
+const swiperCenter = new Swiper('.center-slider', {
+  speed: 700,
+  loop: true,
+  spaceBetween: 30,
+  
+
+  // breakpoints: {
+  //   991: {
+  //     slidesPerView: 3,
+  //   },
+  // },
+
+  navigation: {
+    nextEl: '.center-button-next',
+    prevEl: '.center-button-prev',
+  },
+});
+
+
 
 const swiper = new Swiper('.advantages-slider', {
   speed: 700,
